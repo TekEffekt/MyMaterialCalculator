cc74a4c835b3077943584c78e2b85a7a44e72c68
Kyle K Zawacki <z95sk8@outlook.com>
2015-10-06 17:54:40 -0500

    Add compatability with all devices

--- app/src/main/java/com/example/kylez/calculator/MainActivity.java
+++ app/src/main/java/com/example/kylez/calculator/MainActivity.java
@@ -1,23 +1,35 @@
 package com.example.kylez.calculator;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.AttributeSet;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.util.TypedValue;
+import android.view.Display;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
 import android.view.animation.AccelerateDecelerateInterpolator;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import io.codetail.animation.SupportAnimator;
 import io.codetail.animation.ViewAnimationUtils;
+import io.codetail.widget.RevealFrameLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,12 +37,45 @@ public class MainActivity extends AppCompatActivity {
     CalculatorModel model;
     Boolean splashVisible = false;
 
+    public void resizeLayouts()
+    {
+//        Display display = getWindowManager().getDefaultDisplay();
+//        DisplayMetrics outMetrics = new DisplayMetrics();
+//        display.getMetrics(outMetrics);
+//
+//        float density  = getResources().getDisplayMetrics().density;
+//        float dpHeight = outMetrics.heightPixels / density;
+//        float dpWidth  = outMetrics.widthPixels / density;
+//
+//        float contentPixels = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, (float)(dpHeight * 0.25), getResources().getDisplayMetrics());
+//        float numpadPixels = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, (float)(dpHeight * 0.73), getResources().getDisplayMetrics());
+//
+//        LinearLayout view_instance = (LinearLayout)findViewById(R.id.what);
+//        ViewGroup.LayoutParams params = view_instance.getLayoutParams();
+//        params.height = (int)contentPixels;
+//        view_instance.setLayoutParams(params);
+//
+//        FrameLayout numpadLayout = (FrameLayout)findViewById(R.id.test);
+//
+//        ViewGroup.LayoutParams params2 = numpadLayout.getLayoutParams();
+//        params2.height = (int)numpadPixels;
+//        numpadLayout.setLayoutParams(params2);
+//
+//        TextView text = (TextView)findViewById(R.id.calc_display);
+//        ViewGroup.LayoutParams params3 = text.getLayoutParams();
+//        params3.height = (int)contentPixels;
+//
+//        text.setTextSize(dpWidth/5);
+//        text.setLayoutParams(params3);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
 
         this.calcDisplay = (TextView)findViewById(R.id.calc_display);
         FloatingActionButton fab = (FloatingActionButton)findViewById(R.id.equalButton);
@@ -43,6 +88,16 @@ public class MainActivity extends AppCompatActivity {
         });
 
         model = new CalculatorModel();
+
+                Display display = getWindowManager().getDefaultDisplay();
+        DisplayMetrics outMetrics = new DisplayMetrics();
+        display.getMetrics(outMetrics);
+
+        float density  = getResources().getDisplayMetrics().density;
+        float dpHeight = outMetrics.heightPixels / density;
+        float dpWidth  = outMetrics.widthPixels / density;
+
+        Log.v("DPs",dpHeight+"");
     }
 
     @Override
@@ -91,6 +146,30 @@ public class MainActivity extends AppCompatActivity {
             removeSplash();
         }
     }
+
+    public void backTouched(View v)
+    {
+        if(model.computeQueue.size() > 0)
+        {
+            model.computeQueue.removeFirst();
+        }
+
+        if(calcDisplay.getText().length() > 0)
+        {
+            calcDisplay.setText(((String) calcDisplay.getText()).substring(1));
+        }
+
+        if(calcDisplay.getText().length() == 0)
+        {
+            calcDisplay.setText("0");
+        }
+
+        if(splashVisible)
+        {
+            removeSplash();
+        }
+    }
+
 
     public void performSplash()
     {
